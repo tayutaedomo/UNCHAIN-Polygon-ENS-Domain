d5b6165cafc8d760e0e713521faaadb11cf6d9ac
tayutaedomo <tayutaedomo@gmail.com>
2023-08-14 00:03:36 +0900

    Section 1 Lesson 2 is completed

---
+++ packages/contract/contracts/Domains.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import "hardhat/console.sol";
+
+contract Domains {
+    constructor() {
+        console.log("THIS IS MY DOMAINS CONTRACT. NICE.");
+    }
+}
