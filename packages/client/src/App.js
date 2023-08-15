1819fe96af033106720fcdc2f06c3df6bb8b7d85
tayutaedomo <tayutaedomo@gmail.com>
2023-08-15 21:36:45 +0900

    Section 2 Lesson 2 is completed

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,13 +1,19 @@
 import { React, useEffect, useState } from "react";
 import "./styles/App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
+import { ethers } from "ethers";
+import contractAbi from "./utils/Domains.json";
 
 // Constants
 const TWITTER_HANDLE = "_UNCHAIN";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS;
+const tld = ".banana";
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [domain, setDomain] = useState("");
+  const [record, setRecord] = useState("");
 
   const connectWallet = async () => {
     try {
@@ -50,6 +56,60 @@ const App = () => {
     }
   };
 
+  const mintDomain = async () => {
+    if (!domain) {
+      return;
+    }
+
+    if (domain.length < 3) {
+      alert("Domain must be at least 3 characters long");
+      return;
+    }
+
+    const price =
+      domain.length === 3 ? "0.005" : domain.length === 4 ? "0.003" : "0.001";
+    console.log("Minting domain", domain, "for price", price);
+
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          contractAbi.abi,
+          signer
+        );
+
+        console.log("Going to pop wallet now to pay gas...");
+        const tx = await contract.register(domain, {
+          value: ethers.utils.parseEther(price),
+        });
+        const receipt = await tx.wait();
+
+        if (receipt.status === 1) {
+          console.log(
+            "Domain minted! https://mumbai.polygonscan.com/tx/" + tx.hash
+          );
+
+          const tx2 = await contract.setRecord(domain, record);
+          await tx2.wait();
+
+          console.log(
+            "Record set! https://mumbai.polygonscan.com/tx/" + tx2.hash
+          );
+
+          setDomain("");
+          setRecord("");
+        } else {
+          alert("Transaction failed! Plase try again.");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
@@ -69,6 +129,46 @@ const App = () => {
     </div>
   );
 
+  const renderInputForm = () => {
+    return (
+      <div className="form-container">
+        <div className="first-row">
+          <input
+            type="text"
+            value={domain}
+            placeholder="domain"
+            onChange={(e) => setDomain(e.target.value)}
+          />
+          <p className="tld">{tld}</p>
+        </div>
+
+        <input
+          type="text"
+          value={record}
+          placeholder="What are bananas for you?"
+          onChange={(e) => setRecord(e.target.value)}
+        />
+
+        <div className="button-container">
+          <button
+            className="cta-button mint-button"
+            disabled={null}
+            onClick={mintDomain}
+          >
+            Mint
+          </button>
+          <button
+            className="cta-button mint-button"
+            disabled={true}
+            onClick={null}
+          >
+            Set data
+          </button>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
       <div className="container">
@@ -82,6 +182,7 @@ const App = () => {
         </div>
 
         {!currentAccount && renderNotConnectedContainer()}
+        {currentAccount && renderInputForm()}
 
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
