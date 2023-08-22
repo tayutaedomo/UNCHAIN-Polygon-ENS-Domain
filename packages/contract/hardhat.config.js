3464f36293ed5389cf4ae052a03a3771e2634441
tayutaedomo <tayutaedomo@gmail.com>
2023-08-23 01:56:23 +0900

    Section 4 Lesson 1, Add verify

--- packages/contract/hardhat.config.js
+++ packages/contract/hardhat.config.js
@@ -1,12 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const { MUMBAI_URL, PRIVATE_KEY, POLYGONSCAN_API } = process.env;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
   networks: {
     mumbai: {
-      url: process.env.MUMBAI_URL,
-      accounts: [process.env.PRIVATE_KEY],
+      url: MUMBAI_URL,
+      accounts: [PRIVATE_KEY],
     },
+  },
+  etherscan: {
+    apiKey: POLYGONSCAN_API,
   },
 };
